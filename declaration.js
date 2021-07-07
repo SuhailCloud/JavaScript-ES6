df3ac49a929079ca17ebb59d6fb8a5d81df72194
Safnaj <safnaj99@gmail.com>
2021-07-07 23:23:29 +0530

    declaration

---
+++ declaration.js
@@ -0,0 +1,25 @@
+// VAR
+
+var greeting = "Hello";
+var greeting = "World";
+
+var hello = "Hello";
+hello = "Hello World";
+
+
+// LET
+
+let greeting = "say Hi";
+greeting = "say Hello instead";
+
+let greeting = "say Hi";
+let greeting = "say Hello instead"; // error: Identifier 'greeting' has already been declared
+
+
+// CONST
+
+const greeting = "say Hi";
+greeting = "say Hello instead";// error: Assignment to constant variable. 
+
+const greeting = "say Hi";
+const greeting = "say Hello instead";// error: Identifier 'greeting' has already been declared
