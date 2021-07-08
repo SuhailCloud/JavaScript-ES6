5674539779c15c7a658b2d3a4132edca3b0e6f08
Safnaj <safnaj99@gmail.com>
2021-07-08 21:52:49 +0530

    template literals

---
+++ template.js
@@ -0,0 +1,9 @@
+const person = {
+    fname: "Ahamed",
+    lname: "Safnaj",
+    age: 25,
+    country: "Sri Lanka"
+}
+
+let intro = `Congratulations ${person.fname} ${person.lname} for turning ${person.age}`;
+console.log(intro);
