4be217eb6b6a4b8f47ee660854a4f18a66dc7693
Safnaj <safnaj99@gmail.com>
2021-07-15 19:45:49 +0530

    arrow function

--- arrow.js
+++ arrow.js
@@ -1,12 +1,12 @@
 function area(x, y) {
-    return x*y;
+  return x * y;
 }
 
 const area2 = (x, y) => {
-    return x*y;
-}
+  return x * y;
+};
 
-const area3 = (x,y) => x*y;
+const area3 = (x, y) => x * y;
 
 console.log(area(10, 12));
 console.log(area2(10, 12));
