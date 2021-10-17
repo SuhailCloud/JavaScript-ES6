f4374a274e48288997769b4696ecdf1fa92e610f
Suhail Jamaldeen <suhail_js@yahoo.com>
2021-10-18 01:36:05 +0530

    Adding Values to the arrow

--- arrow.js
+++ arrow.js
@@ -8,6 +8,6 @@ const area2 = (x, y) => {
 
 const area3 = (x, y) => x * y;
 
-console.log(area(10, 12));
-console.log(area2(10, 12));
-console.log(area3(10, 12));
+console.log(area(121, 12));
+console.log(area2(122, 12));
+console.log(area3(12, 11));
