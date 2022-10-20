25eee20ef6eedba786711d09042b30cbfa6ba41a
MinhaYoosuf <minha@titumlk.onmicrosoft.com>
2022-10-20 21:06:40 +0530

    Added Function3

--- arrow2.js
+++ arrow2.js
@@ -6,9 +6,14 @@ const print = {
     function2: () => {
         console.log('This is the Function 2', this);
     }
+
+    function3: () => {
+        console.log('This is the Function 3', this);
+    }
 }
 
 print.function1();
 print.function2();
+print.function3();
 
 //this keyword wont work with arrow function. 
