b0b208731b2b7b24a3ec967ca2e7e4f6b85ae100
MinhaYoosuf <minha@titumlk.onmicrosoft.com>
2022-10-20 21:11:13 +0530

    changes

--- template.js
+++ template.js
@@ -1,9 +1,9 @@
-const person = {
+const person1 = {
     fname: "Ahamed",
     lname: "Safnaj",
     age: 25,
     country: "Sri Lanka"
 }
 
-let intro = `Congratulations ${person.fname} ${person.lname} for turning ${person.age}`;
+let intro = `Congratulations ${person1.fname} ${person1.lname} for turning ${person1.age}`;
 console.log(intro);
